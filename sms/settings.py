4796bfa0a2a7cb90b51458dfc7666a98d916bd67
ismail-se <62374544+ismail-se@users.noreply.github.com>
2020-11-06 12:52:30 +0500

    balance set

--- sms/settings.py
+++ sms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'lua!am4gy408($a4&q@x$mv2^rz4w@z2_9xlz*el!#9h!@%lp4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['edeneducation.herokuapp.com', '127.0.0.1']
 
